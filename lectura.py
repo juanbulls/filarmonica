072040561bd1f2e6ad0c6eefd92ed3fe09e8ad5e
juanbulls <juanbulls@yahoo.com>
2018-06-16 11:37:25 -0500

    puliendo

--- lectura.py
+++ lectura.py
@@ -2,6 +2,7 @@ import json
 from pprint import pprint
 with open(r"2011light.json") as f:
 	datos=json.load(f)
-print("se va a leer: \n orchestra\n del primer\n programs\n asi: datos[programs][0][orchesta] omitiendo comillas")
+print("se va a leer: \n orchestra\n del primer\n programs\n asi: datos[programs][0][orchesta] omitiendo comillas\n")
 resultado=datos["programs"][0]["orchestra"]
-input(resultado+"\nSalir?")
+print(resultado+"\n")
+input("Salir?")
