039f943bf6042e42b8c3e4dd30287cbb6670ffea
juanbulls <juanbulls@yahoo.com>
2018-06-16 10:44:31 -0500

    Revert "cambio de nombre"
    This reverts commit 6252f73b45298babd3dbcc3ddc2b6ec1beba5245.

--- lectura.py
+++
@@ -1,8 +0,0 @@
-import json
-from pprint import pprint
-with open("data.json") as f:
-	data=json.load(f)
-	pprint(data)
-print("se va a leer: \n id\n del primer\n maps\n asi: miniProgramas[programs][0][id] omitiendo comillas")
-miniProgramas["programs"][0]["id"]
-input("Salir")
