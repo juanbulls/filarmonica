d9b5612e6af705e3f1b8e46e572480cec45ef4f5
juanbulls <juanbulls@yahoo.com>
2018-06-16 11:34:29 -0500

    simplificando lo que no habia simplificado

--- lectura.py
+++ lectura.py
@@ -2,7 +2,6 @@ import json
 from pprint import pprint
 with open(r"2011light.json") as f:
 	datos=json.load(f)
-	pprint(datos)
 print("se va a leer: \n orchestra\n del primer\n programs\n asi: datos[programs][0][orchesta] omitiendo comillas")
 resultado=datos["programs"][0]["orchesta"]
 input(resultado+"\nSalir?")
