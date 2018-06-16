5a276041befebca0676d1ff1ffa512a1ebb395f9
juanbulls <juanbulls@yahoo.com>
2018-06-16 10:58:47 -0500

    la r para que entienda el texo

--- lectura.py
+++ lectura.py
@@ -1,6 +1,6 @@
 import json
 from pprint import pprint
-with open("data.json") as f:
+with open(r"data.json") as f:
 	data=json.load(f)
 	pprint(data)
 print("se va a leer: \n id\n del primer\n maps\n asi: miniProgramas[programs][0][id] omitiendo comillas")
