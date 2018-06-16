1910ac92f59b54ecba20fffed79b66c0bfa0b871
juanbulls <juanbulls@yahoo.com>
2018-06-16 11:32:09 -0500

    complete cambio data a datos

--- lectura.py
+++ lectura.py
@@ -2,7 +2,7 @@ import json
 from pprint import pprint
 with open(r"2011light.json") as f:
 	datos=json.load(f)
-	pprint(data)
+	pprint(datos)
 print("se va a leer: \n orchestra\n del primer\n programs\n asi: miniProgramas[programs][0][id] omitiendo comillas")
-resultado=data["programs"][0]["orchesta"]
+resultado=datos["programs"][0]["orchesta"]
 input(resultado+"\nSalir?")
