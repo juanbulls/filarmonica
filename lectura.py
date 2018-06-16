9bb951cbd1eacec9e9f7781fa35a63d51ca026c2
juanbulls <juanbulls@yahoo.com>
2018-06-16 11:21:33 -0500

    lectura de la base correcta

--- lectura.py
+++ lectura.py
@@ -4,5 +4,5 @@ with open(r"data.json") as f:
 	data=json.load(f)
 	pprint(data)
 print("se va a leer: \n id\n del primer\n maps\n asi: miniProgramas[programs][0][id] omitiendo comillas")
-miniProgramas["programs"][0]["id"]
+data["maps"][0]["id"]
 input("Salir")
